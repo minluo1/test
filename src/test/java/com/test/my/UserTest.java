f09382e3f86f51c5251d86c9f067abdc5d3fda63
minluo <minluo@hengtiansoft.com>
2015-06-11 10:35:32 +0800

    tttt

--- src/test/java/com/test/my/UserTest.java
+++ src/test/java/com/test/my/UserTest.java
@@ -25,7 +25,7 @@ public class UserTest {
     public void addUser() {
         User user = new User();
         user.setNickname("hello");
-        user.setState(20);
+        user.setState(2);
         System.out.println(userService.insertUser(user));
     }
 }
